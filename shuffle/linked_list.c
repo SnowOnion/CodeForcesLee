316ab17e2320dc76c3650ce0f391f050e771a260
SnowOnion <snowonionlee@gmail.com>
2014-10-02 07:28:02 +0800

    ds review

--- shuffle/linked_list.c
+++ shuffle/linked_list.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<memory.h>
 
 //typedef struct _Val{
 //    int value
 //}Val;
+
 
 typedef struct _Node{
     int value;
@@ -14,7 +16,26 @@ Node* newNode(int value){
     Node* nn = (Node*)malloc(sizeof(Node));
     nn->value = value;
     nn->next = NULL;
+    return nn;
 }
+
+/**
+precon: src != NULL
+*/
+void mount(Node* src, Node* tar){
+    src->next=tar;
+}
+
+/**
+true 1 false 0
+*/
+int isLast(Node* node){
+    return node->next == NULL;
+}
+
+/**
+尽量在 LinkedList 操作中屏蔽 Node 的实现 (比如露出next君)
+*/
 
 typedef struct _LinkedList{
     Node* head;
@@ -27,18 +48,72 @@ void initLL(LinkedList* ll){
 void printLL(LinkedList* ll){
     Node* cursor=ll->head;
     while(cursor->next!=NULL){
-        cursor=cursor->next;
+        cursor = cursor->next;
         printf("%d ",cursor->value);
     }
     printf("\n");
 }
 
-void insertToEnd(LinkedList* ll, int value){
-    Node* cursor=ll->head;
-    while(cursor->next!=NULL){
-        cursor=cursor->next;
+Node* findLast(LinkedList* ll){
+    Node* cursor = ll->head;
+    while(cursor->next != NULL){
+        cursor = cursor->next;
     }
-    cursor->next=newNode(value);
+    return cursor;
+}
+
+void insertToEnd(LinkedList* ll, int value){
+    Node* last=findLast(ll);
+    mount(newNode(value),last);
+}
+
+Node* linearSearchPreceeder(LinkedList* list, int tarVal){
+    Node* pre = list->head;
+    while(pre->next != NULL){
+        if(pre->next->value == tarVal){
+            return pre;
+        }
+    }
+    return NULL;
+}
+
+Node* linearSearch(LinkedList* list, int tarVal){
+    Node* pre;
+    if((pre=linearSearchPreceeder(list,tarVal)) != NULL){
+        return pre->next;
+    }
+    else{
+        return NULL;
+    }
+}
+
+void swapNode_inside(Node* preA, Node* preB){
+    Node* a=preA->next;
+    Node* b=preB->next;
+    Node* tmp;
+
+    preA->next=b;
+    preB->next=a;
+
+    tmp=a->next;
+    a->next=b->next;
+    b->next=tmp;
+}
+
+void swapByValue(LinkedList* list, int aVal, int bVal){
+    Node* preA=NULL;
+    Node* preB=NULL;
+    Node* cur=list->head;
+    while(cur->next!=NULL){
+        if(cur->next->value==aVal){
+            preA=cur;
+        }
+        if(cur->next->value==bVal){
+            preB=cur;
+        }
+        cur=cur->next;
+    }
+    swapNode_inside(preA,preB);
 }
 
 int main(){
@@ -52,6 +127,11 @@ int main(){
     insertToEnd(&ll, 4);
 
     printLL(&ll);
+
+    swapByValue(&ll,3,4);
+
+    printLL(&ll);
+
 
     return 0;
 }
