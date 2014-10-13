bbab3bd27e7f9830342ae2ded460c72845cc550f
SnowOnion <snowonionlee@gmail.com>
2014-10-13 12:22:20 +0800

    a AC. 加括号, 加括号, 加括号.

--- #272div2@476/a.py
+++ #272div2@476/a.py
@@ -1,9 +1,11 @@
+# AC # @GET Embrace the "a if p else b" expression!!!
+
 n, m = map(int, raw_input().split(' '))
 ndc = (n / 2) + (n % 2)
 if m == 1:
     print 1
 elif 0 < m and m < ndc:
-    print (ndc / m) * m + m if ndc % m > 0 else 0
+    print (ndc / m) * m + (m if ndc % m > 0 else 0)
 elif ndc <= m and m <= n:
     print m
 else:
